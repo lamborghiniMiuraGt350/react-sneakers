b8d363d3e1bdcc51c41ecc18f64f2a918d2e8d94
lamborghiniMiuraGt350 <uc4iha.sora@gmail.com>
2026-07-01 01:42:30 +0300

    end of markup

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,28 @@
+import { Card } from "./components/Card";
+import { Drawer } from "./components/Drawer";
+import { Header } from "./components/Header";
 
 
 function App() {
   return (
     <div className="wrapper">
-      <header className="header">
-        <div className="headerLeft">
-          <a href="#"><img width={40} height={40} src="/img/logo.png" alt="Logo" />
-            <div className="headerInfo">
-              <h3>Sneakers</h3>
-              <p>The best sneaker store</p>
-            </div>
-          </a>
-        </div>
-        < ul className="headerRight">
-          <li>
-            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M7.54548 18.1818C7.99735 18.1818 8.36366 17.8155 8.36366 17.3636C8.36366 16.9118 7.99735 16.5455 7.54548 16.5455C7.09361 16.5455 6.72729 16.9118 6.72729 17.3636C6.72729 17.8155 7.09361 18.1818 7.54548 18.1818Z" stroke="#9B9B9B" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-              <path d="M16.5455 18.1818C16.9973 18.1818 17.3637 17.8155 17.3637 17.3636C17.3637 16.9118 16.9973 16.5455 16.5455 16.5455C16.0936 16.5455 15.7273 16.9118 15.7273 17.3636C15.7273 17.8155 16.0936 18.1818 16.5455 18.1818Z" stroke="#9B9B9B" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-              <path d="M1 1H4.27273L6.46545 11.9555C6.54027 12.3321 6.7452 12.6705 7.04436 12.9113C7.34351 13.1522 7.71784 13.2801 8.10182 13.2727H16.0545C16.4385 13.2801 16.8129 13.1522 17.112 12.9113C17.4112 12.6705 17.6161 12.3321 17.6909 11.9555L19 5.09091H5.09091" stroke="#9B9B9B" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-            </svg>
-            <span>1205 $</span>
-          </li>
-          <li>
-            <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path fillRule="evenodd" clipRule="evenodd" d="M0 9C0 4.1211 4.1211 0 9 0C13.8789 0 18 4.1211 18 9C18 11.871 16.5672 14.4702 14.4 16.1316V16.2H14.3082C12.807 17.3205 10.9683 18 9 18C7.0317 18 5.193 17.3205 3.6918 16.2H3.6V16.1316C1.4328 14.4702 0 11.8701 0 9ZM6.41112 13.7124C5.93239 14.0975 5.59923 14.6344 5.4666 15.2343C6.5178 15.8436 7.7256 16.2 9 16.2C10.2744 16.2 11.4822 15.8436 12.5334 15.2343C12.4006 14.6344 12.0674 14.0977 11.5887 13.7126C11.11 13.3275 10.5144 13.1169 9.9 13.1157H8.1C7.48557 13.1168 6.88986 13.3272 6.41112 13.7124ZM12.391 12.0705C13.129 12.5617 13.7057 13.2596 14.049 14.0769C15.3693 12.7638 16.2 10.9584 16.2 9C16.2 5.0967 12.9033 1.8 9 1.8C5.0967 1.8 1.8 5.0967 1.8 9C1.8 10.9584 2.6307 12.7638 3.951 14.0769C4.2943 13.2596 4.87104 12.5617 5.60904 12.0705C6.34704 11.5794 7.21351 11.3168 8.1 11.3157H9.9C10.7865 11.3168 11.653 11.5794 12.391 12.0705ZM5.40001 7.2C5.40001 5.148 6.94801 3.6 9.00001 3.6C11.052 3.6 12.6 5.148 12.6 7.2C12.6 9.252 11.052 10.8 9.00001 10.8C6.94801 10.8 5.40001 9.252 5.40001 7.2ZM7.20001 7.2C7.20001 8.2602 7.93981 9 9.00001 9C10.0602 9 10.8 8.2602 10.8 7.2C10.8 6.1398 10.0602 5.4 9.00001 5.4C7.93981 5.4 7.20001 6.1398 7.20001 7.2Z" fill="#9B9B9B" />
-            </svg>
-            <span>Profile</span>
-          </li>
-        </ ul>
-      </header >
+      <Drawer />
+
+
+      <Header />
       <div className="content">
-        <h1 className="content__title">All Sneakers</h1>
+        <div className="content__inner">
+          <h1 className="content__title">All Sneakers</h1>
+          <div className="search-block">
+            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M15.25 15.25L11.8855 11.8795L15.25 15.25ZM13.75 7.375C13.75 9.06576 13.0784 10.6873 11.8828 11.8828C10.6873 13.0784 9.06576 13.75 7.375 13.75C5.68424 13.75 4.06274 13.0784 2.86719 11.8828C1.67165 10.6873 1 9.06576 1 7.375C1 5.68424 1.67165 4.06274 2.86719 2.86719C4.06274 1.67165 5.68424 1 7.375 1C9.06576 1 10.6873 1.67165 11.8828 2.86719C13.0784 4.06274 13.75 5.68424 13.75 7.375V7.375Z" stroke="#E4E4E4" strokeWidth="2" strokeLinecap="round" />
+            </svg>
+            <input type="text" placeholder="Search..." />
+          </div>
+        </div>
         <div className="cards">
-          <div className="card">
+          {/* <div className="card">
+            <div className="favorite"><img src="/img/icons/like-start.svg" alt="" /></div>
             <img width={133} height={112} src="/img/main/Blazer.jpg" alt="Sneaker" />
             <h5>Nike Blazer Mid Suede Men's Sneakers</h5>
             <div className="card__bottom">
@@ -47,8 +37,13 @@ function App() {
                 </svg>
               </button>
             </div>
-          </div>
-          <div className="card">
+          </div> */}
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          {/* <div className="card">
+            <div className="favorite"><img src="/img/icons/like-start.svg" alt="" /></div>
             <img width={133} height={112} src="/img/main/Air.jpg" alt="Sneaker" />
             <h5>Nike Air Max 270 Men's Sneakers</h5>
             <div className="card__bottom">
@@ -65,6 +60,7 @@ function App() {
             </div>
           </div>
           <div className="card">
+            <div className="favorite"><img src="/img/icons/like-start.svg" alt="" /></div>
             <img width={133} height={112} src="/img/main/nike.jpg" alt="Sneaker" />
             <h5>Nike Blazer Mid Suede Men's Sneakers</h5>
             <div className="card__bottom">
@@ -81,6 +77,7 @@ function App() {
             </div>
           </div>
           <div className="card">
+            <div className="favorite"><img src="/img/icons/like-start.svg" alt="" /></div>
             <img width={133} height={112} src="/img/main/puma.jpg" alt="Sneaker" />
             <h5>Puma X Aka Boku Future Rider Sneakers</h5>
             <div className="card__bottom">
@@ -95,7 +92,7 @@ function App() {
                 </svg>
               </button>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     </div >
@@ -103,3 +100,5 @@ function App() {
 }
 
 export default App;
+
+
---
+++ src/components/Drawer.jsx
@@ -0,0 +1,50 @@
+export function Drawer() {
+    return (
+        <div className="overlay" style={{ display: 'none' }}>
+            <div className="drawer">
+                <h2>Cart <img className="remove__btn" width={32} height={32} src="/img/icons/btn-remove.svg" alt="button" /></h2>
+
+                <div className="items">
+                    <div className="cart__item">
+                        <div style={{ backgroundImage: 'url(/img/main/Air.jpg)' }} className="cart-item-img">
+                        </div>
+                        <div className="cart__info">
+                            <p>Nike Air Max 270 Men's Sneakers</p>
+                            <p>300 $</p>
+                        </div>
+                        <img className="remove__btn" src="/img/icons/btn-remove.svg" alt="button" />
+                    </div>
+                    <div className="cart__item">
+                        <div style={{ backgroundImage: 'url(/img/main/Air.jpg)' }} className="cart-item-img">
+                        </div>
+                        <div className="cart__info">
+                            <p>Nike Air Max 270 Men's Sneakers</p>
+                            <p>300 $</p>
+                        </div>
+                        <img className="remove__btn" src="/img/icons/btn-remove.svg" alt="button" />
+                    </div>
+                </div>
+
+                <div className="cartTotalBlock">
+                    <ul className="">
+                        <li >
+                            <span>Total:</span>
+                            <div></div>
+                            <p>600$</p>
+                        </li>
+                        <li >
+                            <span>Tax 5%:</span>
+                            <div></div>
+                            <p>30$</p>
+                        </li>
+                    </ul>
+                    <a href="#">Place order <svg width="16" height="14" viewBox="0 0 16 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M1 7H14.7143" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                        <path d="M8.71436 1L14.7144 7L8.71436 13" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                    </svg></a>
+                </div>
+
+            </div>
+        </div>
+    )
+}
